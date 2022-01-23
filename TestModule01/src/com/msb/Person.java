752b5cd51c0f7a51ee66f1a1991a45ef126100e8
Rongshenshi <shirongshen136@gmail.com>
2022-01-23 14:18:10 +0900

    创建了Person类

---
+++ TestModule01/src/com/msb/Person.java
@@ -0,0 +1,11 @@
+package com.msb;
+
+/**
+ * @version v1.0
+ * @Desccription: TODO（一句话描述该类的功能）
+ * @Date: 2022-01-23 14:17:19
+ * @Author: 石荣申
+ */
+public class Person {
+    private String name;
+}
